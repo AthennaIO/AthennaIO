45f51d307ae85ec8055c8420fe83705f581d162a
João Lenon <lenonSec7@gmail.com>
2022-04-21 22:31:24 -0300

    chore: update dependencies

--- app/Console/Kernel.ts
+++ app/Console/Kernel.ts
@@ -7,7 +7,11 @@ export class Kernel extends ConsoleKernel {
    * @return void
    */
   protected commands = [
+    import('@athenna/artisan/src/Commands/List/Make'),
+    import('@athenna/artisan/src/Commands/List/Route'),
+    import('@athenna/artisan/src/Commands/List/Eslint'),
     import('@athenna/artisan/src/Commands/Eslint/Fix'),
+    import('@athenna/artisan/src/Commands/Route/List'),
     import('@athenna/artisan/src/Commands/Make/Facade'),
     import('@athenna/artisan/src/Commands/Make/Command'),
     import('@athenna/artisan/src/Commands/Make/Service'),
