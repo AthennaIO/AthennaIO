a9843fe55da6e996b9b6307a835ab3f521719eab
João Lenon <lenonSec7@gmail.com>
2022-10-27 14:47:43 -0300

    chore(config): add protected commands engine to artisan

--- artisan.js
+++ artisan.js
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 
-import { Ignite } from '@athenna/core'
 import { install } from 'source-map-support'
+import { Ignite, ProviderHelper } from '@athenna/core'
 
 async function main() {
   install()
 
   process.env.BOOT_LOGS = 'false'
-  process.env.IS_ARTISAN = 'true'
 
   const application = await new Ignite().fire()
   const artisan = await application.bootArtisan()
 
   await artisan.main('Artisan')
+
+  if (!Config.get('app.protectedCommands').includes(process.argv[2])) {
+    await ProviderHelper.shutdownAll(Env('SHUTDOWN_LOGS', false))
+  }
 }
 
 main().catch()
