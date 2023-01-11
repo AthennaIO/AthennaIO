92eecc31eb90cc23af404b46762851f8568b9a9e
João Lenon <lenonSec7@gmail.com>
2023-01-11 11:43:07 -0300

    chore(boot): remove \n and add IS_ARTISAN env

--- bootstrap/main.js
+++ bootstrap/main.js
@@ -4,6 +4,9 @@ import { install } from 'source-map-support'
 async function main() {
   install()
 
+  process.env.IS_ARTISAN = 'false'
+  process.env.BOOT_LOGS = 'true'
+
   const application = await new Ignite().fire()
 
   await application.bootArtisan()
