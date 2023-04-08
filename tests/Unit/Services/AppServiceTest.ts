4970972a205c451d662453ce60519967a22a4b0d
João Lenon <lenonSec7@gmail.com>
2023-04-08 00:16:51 -0300

    chore(npm): update dependencies

--- tests/Unit/Services/AppServiceTest.ts
+++ tests/Unit/Services/AppServiceTest.ts
@@ -11,7 +11,7 @@ export default class AppServiceTest {
   public async shouldReturnThePayloadFromFindOneMethod({ assert }: TestContext) {
     const appService = new AppService()
 
-    const { name, version, description, source } = await appService.findOne()
+    const { name, version, description, source } = appService.findOne()
 
     assert.equal(name, '@athenna/athenna')
     assert.equal(version, '1.0.0')
