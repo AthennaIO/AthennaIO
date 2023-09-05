c988763cb0766023f16bc744e615324757b166c6
João Lenon <lenonSec7@gmail.com>
2023-09-05 23:34:30 +0100

    chore(npm): update dependencies

--- tests/e2e/app.command.test.ts
+++ tests/e2e/app.command.test.ts
@@ -3,12 +3,12 @@ import { BaseCliTest } from '@athenna/core/testing/BaseCliTest'
 
 export default class AppCommandTest extends BaseCliTest {
   @Test()
-  public async shouldBeAbleToGetTheStdoutOfAppCommand({ assert }: Context) {
-    const { stdout, stderr } = await this.execute('app')
+  public async shouldBeAbleToGetTheStdoutOfAppCommand({ command }: Context) {
+    const output = await command.run('app')
 
-    assert.equal(stderr, '')
-    assert.isTrue(stdout.includes(`Application information's:`))
-    assert.isTrue(stdout.includes('@athenna/athenna'))
-    assert.isTrue(stdout.includes('Athenna is awesome!'))
+    output.assertSucceeded()
+    output.assertLogged(`Application information's:`)
+    output.assertLogged('@athenna/athenna')
+    output.assertLogged('Athenna is awesome!')
   }
 }
