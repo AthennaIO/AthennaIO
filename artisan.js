156b217deca4eae59edd5371d5d4981a36095546
João Lenon <lenonSec7@gmail.com>
2023-10-15 18:10:31 +0100

    chore(npm): improve codebase

--- artisan.js
+++ artisan.js
@@ -3,9 +3,5 @@
 import { Exec } from '@athenna/common'
 
 Exec.artisan('./bootstrap/artisan.js', {
-  nodeOptions: [
-    '--enable-source-maps',
-    '--import=@athenna/tsconfig',
-    '--experimental-import-meta-resolve'
-  ]
+  nodeOptions: ['--enable-source-maps', '--import=@athenna/tsconfig']
 })
