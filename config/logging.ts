c632bae199c4517d981cb4363d1b424eb6ca62bf
jlenon7 <lenonSec7@gmail.com>
2024-04-27 13:07:57 +0100

    chore(npm): update dependencies

--- config/logging.ts
+++ config/logging.ts
@@ -41,6 +41,13 @@ export default {
       formatter: 'simple'
     },
 
+    exception: {
+      level: 'trace',
+      formatter: 'none',
+      driver: 'console',
+      streamType: 'stderr'
+    },
+
     discard: {
       driver: 'null'
     },
