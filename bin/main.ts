6ada255668a1c977c129c1683e42de85f7f55c01
João Lenon <lenonsec7@gmail.com>
2024-08-26 14:24:26 +0100

    chore(npm): update dependencies

--- bin/main.ts
+++ bin/main.ts
@@ -9,7 +9,7 @@ sourceMapSupport.install({ handleUncaughtExceptions: false })
 
 const ignite = await new Ignite().load(import.meta.url, {
   bootLogs: false,
-  athennaRcPath: 'bin/.athennarc.prod.json'
+  athennaRcPath: '.athennarc.prod.json'
 })
 
 await ignite.console(process.argv, { displayName: 'Athenna' })
