bbda8864b969ffa832cca6bd8b8feac8c53088f9
jlenon7 <lenonSec7@gmail.com>
2025-01-16 11:39:08 -0300

    chore(npm): update dependencies

--- src/routes/http.ts
+++ src/routes/http.ts
@@ -3,7 +3,7 @@ import { Route } from '@athenna/http'
 Route.get('*', 'AppController.index')
 
 Route.group(() => {
-  Route.get('/', ({ response }) => {
-    response.status(200).send({ message: 'Hello World!' })
+  Route.get('/', () => {
+    return { message: 'Hello World!' }
   })
 }).prefix('/api/v1')
