b597206723ac74f021b2466820abdddb8f887ff8
jlenon7 <lenonSec7@gmail.com>
2025-01-16 11:55:36 -0300

    chore(npm): update dependencies

--- src/http/controllers/app.controller.ts
+++ src/http/controllers/app.controller.ts
@@ -1,13 +1,24 @@
 import { React } from '@athenna/vite'
+import { Inject } from '@athenna/ioc'
+import { AppService } from '#src/services/app.service'
 import { Controller, type Context } from '@athenna/http'
 
 @Controller()
 export class AppController {
+  @Inject()
+  private readonly appService: AppService
+
   public async index({ request, response }: Context) {
     const { createApp } = await React.loadEntrypoint()
 
     const element = await React.renderComponent(createApp(request.baseUrl))
 
     return response.view('index', { element })
+  }
+
+  public async show({ response }: Context) {
+    const data = this.appService.findOne()
+
+    return response.status(200).send(data)
   }
 }
