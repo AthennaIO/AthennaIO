ea7aeeefd4dae2e2c51be1211314eccb55c0713d
jlenon7 <lenonSec7@gmail.com>
2025-01-16 11:51:20 -0300

    chore(npm): update dependencies

--- src/http/controllers/app.controller.ts
+++ src/http/controllers/app.controller.ts
@@ -1,8 +1,19 @@
+import { Inject } from '@athenna/ioc'
+import { AppService } from '#src/services/app.service'
 import { Controller, type Context } from '@athenna/http'
 
 @Controller()
 export class AppController {
+  @Inject()
+  private readonly appService: AppService
+
   public async index({ response }: Context) {
     return response.view('index')
+  }
+
+  public async show({ response }: Context) {
+    const data = this.appService.findOne()
+
+    return response.status(200).send(data)
   }
 }
--- src/routes/http.ts
+++ src/routes/http.ts
@@ -3,7 +3,5 @@ import { Route } from '@athenna/http'
 Route.get('*', 'AppController.index')
 
 Route.group(() => {
-  Route.get('/', () => {
-    return { message: 'Hello World!' }
-  })
+  Route.get('/', 'AppController.show')
 }).prefix('/api/v1')
